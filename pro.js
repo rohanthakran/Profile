6a29e7071d39195809aa6911e3dd23fbe21dab7a
Rohan <31851961+rohanthakran@users.noreply.github.com>
2019-12-29 11:59:12 +0530

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,10 @@
+function drop(){
+    var x = document.getElementById("nav");
+    if(x.className ==="topnav"){
+        x.className +=  " responsive";
+    }
+    else{
+        x.className ="topnav";
+    }
+    
+}
